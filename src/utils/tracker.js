1734fc4700745c104aee154c5eb3192fa2af7083
costanza <rodric@usi.ch>
2025-04-15 08:58:47 +0200

    working on tracker

--- src/utils/tracker.js
+++ src/utils/tracker.js
@@ -1,19 +1,15 @@
-
 const tracker = {
     userCode: localStorage.userCode,
-    totalSessionClicks: [],
+    totalSessionClicks: [], // { time, page }
     islandClickOrder: [],
     islandCompletionOrder: [],
-    queriesPerTask: {}, // {islandId: number}
-    serpClicks: [],
+    queriesPerIsland: {}, // { islandId: number }
+    queryTermsPerIsland: {}, // { islandId: number of terms }
+    serpResults: [], // { title, snippet, position, clicked, clickOrder, timeSpent }
+    serpClickOrderCounter: 1,
     sessionStart: Date.now(),
     firstClickTime: null,
-    viewedResults: [], // { title, snippet, clicked }
-    queryLengths: [],
-    serpPagesViewed: 0,
-    pageViewDurations: [],
-    domains: new Set(),
-    pagesViewed: new Set(),
+    totalResultsClicked: 0,
 
     recordSessionClick(page) {
         const now = Date.now();
@@ -30,48 +26,53 @@ const tracker = {
     },
 
     recordQuery(islandId, query) {
-        this.queriesPerTask[ islandId ] = (this.queriesPerTask[ islandId ] || 0) + 1;
-        this.queryLengths.push(query.split(" ").length);
+        this.queriesPerIsland[ islandId ] = (this.queriesPerIsland[ islandId ] || 0) + 1;
+        const terms = query.trim().split(/\s+/).length;
+        this.queryTermsPerIsland[ islandId ] = (this.queryTermsPerIsland[ islandId ] || 0) + terms;
     },
 
-    recordSERPClick(position, domain, page, time) {
-        this.serpClicks.push({ position, domain, page, time });
-        this.domains.add(domain);
-        this.pagesViewed.add(page);
+    recordSearchResult({ title, snippet, position }) {
+        this.serpResults.push({
+            title,
+            snippet,
+            position,
+            clicked: false,
+            clickOrder: null,
+            timeSpent: null,
+        });
     },
 
-    recordSearchResult(title, snippet, clicked = false) {
-        this.viewedResults.push({ title, snippet, clicked });
-    },
-
-    recordPageView(page, duration) {
-        this.pageViewDurations.push({ page, duration });
-    },
-
-    recordSERPPageViewed() {
-        this.serpPagesViewed += 1;
+    recordSERPClick(position, timeSpent) {
+        const result = this.serpResults.find(r => r.position === position);
+        if (result && !result.clicked) {
+            result.clicked = true;
+            result.clickOrder = this.serpClickOrderCounter++;
+            result.timeSpent = timeSpent;
+            this.totalResultsClicked += 1;
+        }
     },
 
     getSessionLength() {
-        return Date.now() - this.sessionStart;
+        return Math.round((Date.now() - this.sessionStart) / 1000); // in seconds
+    },
+
+    getTimeBeforeFirstClick() {
+        return this.firstClickTime ? Math.round((this.firstClickTime - this.sessionStart) / 1000) : null;
     },
 
     exportData() {
         return {
             userCode: this.userCode,
+            totalClicks: this.totalSessionClicks.length,
             clicks: this.totalSessionClicks,
             islandClickOrder: this.islandClickOrder,
             islandCompletionOrder: this.islandCompletionOrder,
-            queriesPerTask: this.queriesPerTask,
-            serpClicks: this.serpClicks,
-            sessionLength: this.getSessionLength(),
-            timeBeforeFirstClick: this.firstClickTime ? this.firstClickTime - this.sessionStart : null,
-            viewedResults: this.viewedResults,
-            queryLengths: this.queryLengths,
-            serpPagesViewed: this.serpPagesViewed,
-            pageViewDurations: this.pageViewDurations,
-            numberOfDomains: this.domains.size,
-            numberOfViewedPages: this.pagesViewed.size,
+            queriesPerIsland: this.queriesPerIsland,
+            queryTermsPerIsland: this.queryTermsPerIsland,
+            serpResults: this.serpResults,
+            totalResultsClicked: this.totalResultsClicked,
+            sessionLengthSeconds: this.getSessionLength(),
+            timeBeforeFirstClickSeconds: this.getTimeBeforeFirstClick(),
         };
     },
 };
